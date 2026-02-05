3f5b2b98c1dbd790fa8b23c53d00649cda3e9f2e
“gurpreetbeniwalgit config --global user.email gurpreetbeniwal1731386@gmail.com” <gurpreetbeniwal1731386@gmail.comgit config --global user.email gurpreetbeniwal1731386@gmail.com>
2026-02-05 14:52:00 +0530

    ok

--- config/razorpay.js
+++
@@ -1,9 +0,0 @@
-const Razorpay = require('razorpay');
-require('dotenv').config();
-
-const razorpay = new Razorpay({
-    key_id: "rzp_live_SAWrANbkbWONmt",
-    key_secret: "SbJ3eWEcqonP3rgN3z1jb4NX"
-});
-
-module.exports = razorpay;
--- middleware/adminAuth.js
+++
@@ -1,9 +0,0 @@
-const adminAuth = (req, res, next) => {
-  if (req.session && req.session.admin) {
-    return next();
-  } else {
-    return res.redirect('/admin/login');
-  }
-};
-
-module.exports = adminAuth;
